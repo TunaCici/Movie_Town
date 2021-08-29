1fb5639f59eeebd0c4f6903cc166c73b62a6cc27
tunacici <tunacici7@gmail.com>
2021-08-29 20:45:06 +0300

    Added login page and modified main flask app.

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ from flask import Flask
 from flask import url_for
 from flask import render_template
 from flask import request
-
-from markupsafe import escape
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("hello.html")
+def home():
+    return render_template("home.html")
+
+@app.route("/sign-up", methods=['GET', 'POST'])
+def sign_up():
+    if request.method == "POST":
+        print("getting post")
+        return redirect(url_for("home"))
+
+    return render_template("sign-up.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        print("getting post")
+        return redirect(url_for("home"))
+
+    return render_template("login.html")
