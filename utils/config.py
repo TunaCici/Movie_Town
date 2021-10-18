ee35980aa948741ae4d9c65df5607cfb978381aa
tunacici <tunacici7@gmail.com>
2021-10-19 00:14:11 +0300

    New wsgi and docker files.

--- utils/config.py
+++ utils/config.py
@@ -1,8 +1,3 @@
-import os
-
-# Project
-PROJECT_DIR = "/home/tunac/movie_town/"
-
 # Logger
 LOG_FORMAT = "%(asctime)s [%(levelname)s] [%(name)s] %(message)s"
-LOG_FILE_DIR = PROJECT_DIR + "logs/movie_town_log.txt"
+LOG_FILE_DIR = "./logs/movie_town_log.txt"
