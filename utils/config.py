28db488c23d9c353f58330be0b54523d3535c796
tunacici <tunacici7@gmail.com>
2021-10-19 00:18:44 +0300

    Another one.

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 import os
 
-PROJECT_DIR = os.getcwd()
+PROJECT_DIR = os.getcwd() + "/"
 
 # Logger
 LOG_FORMAT = "%(asctime)s [%(levelname)s] [%(name)s] %(message)s"
-LOG_FILE_DIR = PROJECT_DIR + "/logs/movie_town_log.txt"
+LOG_FILE_DIR = PROJECT_DIR + "logs/movie_town_log.txt"
